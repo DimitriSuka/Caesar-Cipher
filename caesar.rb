3dc6afec27f4be740e8723c3205ed15924810f74
DimitriSuka <dimitri_suka@protonmail.ch>
2020-08-15 05:05:52 +0930

    yee yee bois

---
+++ caesar.rb
@@ -0,0 +1,28 @@
+def caesar_cipher(input, shifter)
+    lowercase_letters = ('a'..'z').to_a
+    uppercase_letters = ('A'..'Z').to_a
+
+    ciphered_array = input.split("").map do |char|
+        lower_index = lowercase_letters.index(char)
+        upper_index = uppercase_letters.index(char)
+
+        if lower_index
+            char = lowercase_letters[(lower_index+shifter) % 26]
+        elsif upper_index
+            char = uppercase_letters[(upper_index+shifter) % 26]
+        else char = char
+        end
+    end
+
+    p ciphered_array.join("")
+end
+
+
+puts "Please enter a string"
+str = gets.chomp.to_s
+
+puts "Please enter a number key"
+num = gets.chomp.to_i
+
+
+caesar_cipher(str, num)
